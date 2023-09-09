d7bdb1aa8c4cabfc89d4cc3f3e12de5751b36e0a
SerafimArts <nesk@xakep.ru>
2023-09-09 16:42:41 +0000

    Add dispatcher component

--- src/Attribute/OnMouseButtonEvent.php
+++ src/Attribute/OnMouseButtonEvent.php
@@ -4,21 +4,31 @@ declare(strict_types=1);
 
 namespace Bic\Mouse\Attribute;
 
+use Bic\Contracts\Event\MatcherInterface;
+use Bic\Mouse\ButtonInterface;
 use Bic\Mouse\Event\MouseButtonEvent;
 
 /**
  * @template TEvent of MouseButtonEvent
  *
  * @template-extends OnMouseEvent<TEvent>
+ * @template-implements MatcherInterface<TEvent>
  */
 #[\Attribute(\Attribute::TARGET_METHOD | \Attribute::IS_REPEATABLE)]
-class OnMouseButtonEvent extends OnMouseEvent
+class OnMouseButtonEvent extends OnMouseEvent implements MatcherInterface
 {
     /**
      * @param class-string<TEvent> $event
      */
-    public function __construct(string $event = MouseButtonEvent::class)
-    {
+    public function __construct(
+        string $event = MouseButtonEvent::class,
+        public readonly ?ButtonInterface $button = null,
+    ) {
         parent::__construct($event);
+    }
+
+    public function match(object $event): bool
+    {
+        return $this->button === null || $event->button === $this->button;
     }
 }
--- src/Attribute/OnMouseWheelEvent.php
+++ src/Attribute/OnMouseWheelEvent.php
@@ -4,16 +4,25 @@ declare(strict_types=1);
 
 namespace Bic\Mouse\Attribute;
 
+use Bic\Contracts\Event\MatcherInterface;
 use Bic\Mouse\Event\MouseWheelEvent;
+use Bic\Mouse\Wheel;
 
 /**
  * @template-extends OnMouseEvent<MouseWheelEvent>
+ * @template-implements MatcherInterface<MouseWheelEvent>
  */
 #[\Attribute(\Attribute::TARGET_METHOD | \Attribute::IS_REPEATABLE)]
-class OnMouseWheelEvent extends OnMouseEvent
+class OnMouseWheelEvent extends OnMouseEvent implements MatcherInterface
 {
-    public function __construct()
-    {
+    public function __construct(
+        public readonly ?Wheel $wheel = null,
+    ) {
         parent::__construct(MouseWheelEvent::class);
+    }
+
+    public function match(object $event): bool
+    {
+        return $this->wheel === null || $event->wheel === $this->wheel;
     }
 }
